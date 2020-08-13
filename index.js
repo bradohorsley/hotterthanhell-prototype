be600d5e5dea835ac8a2e0d288b006659f07fa04
bradohorsley <bradohorsley@aim.com>
2020-08-13 19:06:08 +0100

    2 files modified

--- index.js
+++ index.js
@@ -97,21 +97,26 @@ $(function() {
       switch(error.code) {
     case error.PERMISSION_DENIED:
       console.log("User denied the request for Geolocation.");
+      $('#getlocation').modal();
       break;
     case error.POSITION_UNAVAILABLE:
       console.log("Location information is unavailable.");
+      $('#getlocation').modal();
       break;
     case error.TIMEOUT:
       console.log("The request to get user location timed out.");
+      $('#getlocation').modal();
       break;
     case error.UNKNOWN_ERROR:
       console.log("An unknown error occurred.");
+      $('#getlocation').modal();
       break;
   }
     });
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+    console.log("Geolocation is not supported by this browser.");
     console.log(navigator.error);
+      $('#getlocation').modal();
   }
 
   //Change the temp from C to F
